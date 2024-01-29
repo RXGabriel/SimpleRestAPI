c21181b60735adb39dd7da55a19d4085ef8168cc
RXGabriel <gabriel2005rf@gmail.com>
2024-01-29 15:37:35 -0300

    base

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,7 +1,4 @@
 import {Request, Response} from 'express'
-import { Sequelize } from 'sequelize'
-import {Phrase} from '../models/Phrase'
-import {sequelize} from '../instance/mysql'
 
 export const ping = (req: Request, res: Response) => {
   res.json({pong: true})
@@ -16,71 +13,7 @@ export const nome = (req: Request, res: Response) => {
   let nome : string = req.params.nome
   res.json({nome: `Você enviou o nome ${nome}`})
 }
-
-export const createPhrase = async (req: Request, res: Response) => {
-  try {
-    const { author, txt } = req.body;
-    const newPhrase = await Phrase.create({ author, txt });
-
-    res.status(201)
-    res.json({ id: newPhrase.id, author, txt });
-
-    
-    await sequelize.authenticate()
-  } catch (error) {
-    console.error('Error creating phrase:', error);
-  }
-};
-
-export const listPhrases = async (req: Request, res: Response) => {
-  let list = await Phrase.findAll()
-  
-  res.json({list})
-}
-
-export const getPhrase = async (req: Request, res: Response) => {
-  let {id} = req.params
-  let phrase = await Phrase.findByPk(id)
-
-  if(phrase){
-    res.json({phrase})
-  } else{
-    res.json({error: 'Frase não encontrada'})
-  }
-}
-
-export const updatePhrase = async (req: Request, res: Response) => {
-  let {id} = req.params
-  let {author, txt} = req.body
-  let phrase = await Phrase.findByPk(id)
-
-  if(phrase){
-    phrase.author = author
-    phrase.txt = txt
-
-    await phrase.save()
-    res.json({phrase})  
-  } else{
-   res.json({error: 'Frase não encontrada'})
-  }
-}
-
-export const deletePhrase = async (req: Request, res: Response) => {
-  let {id} = req.params
-  
-  await Phrase.destroy({where: {id}})
-}
-
-export const randomPhrase = async (req: Request, res: Response) => {
-  let phrase = await Phrase.findOne({
-    order: [
-      Sequelize.fn('RAND'),
-    ]
-  })
-
-  if(phrase){
-    res.json({phrase})
-  } else{
-    res.json({error: 'Frase sem registro'})
-  }
+export const updateFile = async (req: Request, res: Response) => {
+  console.log(req.file)
+  res.json({})  
 }
