95dd98250d0a6e60ab7663a5b7233ad05bd2962d
RXGabriel <gabriel2005rf@gmail.com>
2024-01-30 09:36:27 -0300

    Handling upload errors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
-import express,{ Request, Response } from 'express';
+import express,{ Request, Response,ErrorRequestHandler } from 'express';
 import path from 'path'; 
 import dotenv from 'dotenv';
 import cors from 'cors'
+import { MulterError } from 'multer';
 import apiRoutes from './routes/api'
 
 dotenv.config();
@@ -18,5 +19,16 @@ server.use((req:Request, res:Response)=>{
   res.json({error: 'Page not found'})
 })
 
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  res.status(400);
+
+  if (err instanceof MulterError) {
+    res.json({ error: err.code });
+  } else {
+    console.error(err);
+    res.json({ error: 'Error' });
+  }
+};
+server.use(errorHandler)
 
 server.listen(process.env.PORT)
